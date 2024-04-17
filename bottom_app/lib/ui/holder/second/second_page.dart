f45f88484e5267e20abb9d0e1a17e9e39749955f
Pakchan <1625787@donga.ac.kr>
2024-04-17 12:39:49 +0900

    [market] 시작

--- bottom_app/lib/ui/holder/second/second_page.dart
+++ bottom_app/lib/ui/holder/second/second_page.dart
@@ -1,4 +1,7 @@
+import 'package:bottom_app/ui/holder/second/chat_card.dart';
 import 'package:flutter/material.dart';
+
+import 'chat.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({
@@ -7,6 +10,17 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Second Page"));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("채팅"),
+      ),
+      body: ListView(
+        children: List.generate(
+          chats.length,
+          (index) => ChatCard(chat: chats[index]),
+        ),
+      ),
+    );
   }
 }
--- bottom_app/lib/ui/holder/third/third_page.dart
+++ bottom_app/lib/ui/holder/third/third_page.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_app/ui/holder/third/tab.dart';
 import 'package:flutter/material.dart';
 
 class ThirdPage extends StatelessWidget {
@@ -7,6 +8,27 @@ class ThirdPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Third Page"));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("더보기"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: GridView.count(
+          crossAxisCount: 4,
+          children: List.generate(
+            tabs.length,
+            (index) => Column(
+              children: [
+                Icon(tabs[index].icon),
+                SizedBox(height: 5),
+                Text(tabs[index].text),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
