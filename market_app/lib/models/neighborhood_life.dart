1e01b7abb48cb76d026e6318998bb8c9bd0ae40b
Pakchan <1625787@donga.ac.kr>
2024-04-18 10:40:26 +0900

    [market] 동네생활 화면 만들기

---
+++ market_app/lib/models/neighborhood_life.dart
@@ -0,0 +1,23 @@
+class NeighborhoodLife {
+  final String category;
+  final String profileImgUri;
+  final String username;
+  final String location;
+  final String content;
+  final String contentImgUri;
+  final int commentCount;
+  final int authCount;
+  final String date;
+
+  NeighborhoodLife({
+    required this.category,
+    required this.profileImgUri,
+    required this.username,
+    required this.location,
+    required this.content,
+    required this.contentImgUri,
+    required this.commentCount,
+    required this.authCount,
+    required this.date,
+  });
+}
